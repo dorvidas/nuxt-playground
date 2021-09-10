7ba10a4a5509bad7886d858872166f75e7546a94
pc-dorvi <dorvidas@gmail.com>
2021-09-10 11:09:42 +0300

    Add input for adding todos

--- components/Todos.vue
+++ components/Todos.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <input v-model="newItem" type="text">
+    <button @click="addItem">Add</button>
     <ul>
       <li v-for="item in todos" :key="item.id">
         {{ item.title }}
@@ -12,11 +14,22 @@
 export default {
   data () {
     return {
+      newItem: '',
       todos: []
     }
   },
   async fetch () {
-    this.todos = await fetch('https://jsonplaceholder.typicode.com/todos/').then(res => res.json())
+    const items = await fetch('https://jsonplaceholder.typicode.com/todos/').then(res => res.json())
+    this.todos = items.slice(0, 20)
+  },
+  methods: {
+    addItem () {
+      this.todos.push({
+        id: this.todos.length,
+        title: this.newItem
+      })
+      this.newItem = ''
+    }
   }
 }
 </script>
